978a020e124cb1a2a81629e19951417f843829ce
mugen <1870282173@qq.com>
2024-12-26 17:57:19 +0800

    提交内容管理模块和代码生成器

---
+++ xuecheng-plus-content/xuecheng-plus-content-model/src/main/java/com/xuecheng/content/model/dto/QueryCourseParamsDto.java
@@ -0,0 +1,21 @@
+package com.xuecheng.content.model.dto;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * @description 课程查询参数Dto
+ * @author mugen
+ */
+ @Data
+ @ToString
+public class QueryCourseParamsDto {
+
+  //审核状态
+  private String auditStatus;
+  //课程名称
+  private String courseName;
+  //发布状态
+  private String publishStatus;
+
+}
